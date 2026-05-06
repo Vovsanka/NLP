7284babaa1515dfed9468daac4a366a11e564163
Vovsanka <drobitkovolodya@gmail.com>
2026-05-06 19:10:03 +0200

    rule and word models & induce accumulators

--- src/induce.py
+++ src/induce.py
@@ -2,15 +2,17 @@ import sys
 
 from parsy import string, regex, seq, forward_declaration
 
-from models import SE, T, NT
+from models import SE, T, NT, R, W
 
-def parsePTB():
+def parse_ptb():
     pass
     # TODO: implement
 
 def induce_grammar(grammar_prefix: str|None = None):
     # rule accumulation
-    # TODO: rules counter (map),  lexicon counter (map), word terminals (set) 
+    rule_counter: dict[R, int] = {}  
+    word_counter: dict[W, int] = {}
+    words: set[T] = {}
     # request the treebank input
     print("\nTraining Treebank: (one tree per line)")
     for line in sys.stdin:
@@ -18,7 +20,7 @@ def induce_grammar(grammar_prefix: str|None = None):
         line = line.rstrip("\n")
         if not line.strip():
             continue  # skip empty lines
-        se: SE = parsePTB(line)
+        se: SE = parse_ptb(line)
         # TODO: accumulate
     
     #TODO: normalize and create grammer (save to 3 files or write to stdout)
--- src/models.py
+++ src/models.py
@@ -1,11 +1,8 @@
 from typing import Union, List
 
-T = Terminal = str # terminal = word
-NT = NonTerminal = str # non-terminal = non-leaf tree node label
-A = Atom = T | NT  # atom = token = terminal or non-terminal
 
 class SymbolicExpression:
-    # label: NT 
+    # label: NonTerminal
     # children: List[Union[Terminal, "SymbolicExpression"]] # [T] or [SE, SE, SE, ...]
 
     def __init__(self, label: NonTerminal, children: List[Union[Terminal, "SymbolicExpression"]]):
@@ -17,9 +14,40 @@ class SymbolicExpression:
         self.label = label
         self.children = children
 
+class Rule:
+    # label: NonTerminal
+    # children: list[NonTerminal]
+
+    def __init__(self, label: NonTerminal, children: list[NonTerminal]):
+        if not isinstance(label, NonTerminal):
+            raise Exception("Parsing error: rule label must be a non-terminal!")
+        if not children:
+            raise Exception("Parsing error: rule contains only one element!")
+
+        self.label = label
+        self.children = children
+
+
+class Word:
+    # label: NonTerminal
+    # word: Terminal
+    
+    def __init__(self, label: NonTerminal, word: Terminal):
+        if not isinstance(label, NonTerminal):
+            raise Exception("Parsing error: token label must be a non-terminal!")
+        if not isinstance(word, Terminal):
+            raise Exception("Parsing error: token word must be a terminal!")
+
+        self.label = label
+        self.word = word
+
+
+T = Terminal = str # terminal = word
+NT = NonTerminal = str # non-terminal = non-leaf tree node label
+A = Atom = T | NT  # atom = token = terminal or non-terminal
 SE = SymbolicExpression # symbolic expression = non-leaf tree node
+R = Rule # grammar rule
+W = Word # word with its grammar label (different labels possible)
 
-# TODO: Rule
 
-# TODO: Lexicon
 

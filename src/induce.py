35c6edfe49d6e9644da2783056c94aa87974c697
Vovsanka <drobitkovolodya@gmail.com>
2026-05-07 03:46:43 +0200

    PCFG induction  (not tested)

--- src/induce.py
+++ src/induce.py
@@ -2,28 +2,138 @@ import sys
 
 from parsy import string, regex, seq, forward_declaration
 
-from models import SE, T, NT, R, W
+from models import SE, T, NT, A, SR, LR
 
-def parse_ptb():
-    pass
-    # TODO: implement
+
+WS = regex(r"\s*") # optional whitespace
+LPAR = string("(") # left paranthesis
+RPAR = string(")") # right paranthesis
+TOKEN = regex(r"[^()\s]+") # Terminal or Non-Terminal (Atom)
+
+SE_PARSER = forward_declaration()
+
+def build_symbolic_expression(parsed) -> SE:
+    label, children = parsed
+    return SE(NT(label), children)
+
+SE_INNER = seq(
+    TOKEN,  
+    (WS >> (SE_PARSER | TOKEN)).many() 
+).map(build_symbolic_expression)
+
+SE_PARSER.become(LPAR >> WS >> SE_INNER << WS << RPAR)
+
+def parse_ptb(tree: str) -> SE:
+    parsed, rest = SE_PARSER.parse_partial(tree)
+
+    if rest.strip():
+        raise Exception(f"Extra content after first tree: {rest!r}")
+
+    return parsed
 
 def induce_grammar(grammar_prefix: str|None = None):
     # rule accumulation
-    rule_counter: dict[R, int] = {}  
-    word_counter: dict[W, int] = {}
-    words: set[T] = {}
+    label_counter: dict[NT, int] = {}
+    sr_counter: dict[SR, int] = {}  
+    lr_counter: dict[LR, int] = {}
+    words: set[T] = set()
+
+    def accumulate_rules_and_words(se: SE):
+        # accumulate the rule label
+        if se.label not in label_counter:
+            label_counter[se.label] = 0
+        label_counter[se.label] += 1
+
+        # lexical rule (NT -> T)
+        if len(se.children) == 1 and isinstance(se.children[0], T):
+            word = se.children[0]
+            words.add(word)
+
+            wr = LR(label=se.label, word=word)
+            if wr not in lr_counter:
+                lr_counter[wr] = 0
+            lr_counter[wr] += 1
+
+            return
+
+        # the children are exclusively SE
+        # syntactic rule (NT -> NT NT NT ...) 
+        rule = SR(
+            label=se.label,
+            child_labels=[c.label for c in se.children]
+        )
+        if rule not in sr_counter:
+            sr_counter[rule] = 0
+        sr_counter[rule] += 1
+
+        # recurse only into SE children
+        for c in se.children:
+            if isinstance(c, SE):
+                accumulate_rules_and_words(c)
+
     # request the treebank input
     print("\nTraining Treebank: (one tree per line)")
+
     for line in sys.stdin:
         # read the line with the tree from stdin
         line = line.rstrip("\n")
         if not line.strip():
-            continue  # skip empty lines
-        se: SE = parse_ptb(line)
-        # TODO: accumulate
+            break  # finish if reached an empty line (or enter pressed)
+        se: SE = parse_ptb(tree=line)
+        # assert str(se) == line
+        # accumulate rules and words
+        accumulate_rules_and_words(se)
+
+    # open the output files or use stdout
+    if grammar_prefix is None:
+        rules_f = sys.stdout
+        lexicon_f   = sys.stdout
+        words_f = sys.stdout
+    else:
+        rules_f = open(grammar_prefix + ".rules", "w")
+        lexicon_f   = open(grammar_prefix + ".lexicon", "w")
+        words_f = open(grammar_prefix + ".words", "w")
     
-    #TODO: normalize and create grammer (save to 3 files or write to stdout)
-    print("\n")
+    # output the resulting PCFG (normalized) to stdout or to specified files
+    # syntactic rules
+    if grammar_prefix is None:
+        print("\nGrammar.rules:")
+    for rule, count in sr_counter.items():
+        rule_str = f"{rule.label} -> "
+        for cl in rule.child_labels:
+            rule_str += f"{cl} "
+
+        # add the rule probability
+        rule_str += str(count/label_counter[rule.label]) 
+
+        # write the syntactic rule
+        rules_f.write(rule_str + "\n")
+
+    # lexical rules
+    if grammar_prefix is None:
+        print("\nGrammar.lexicon:")
+    for rule, count in lr_counter.items():
+        rule_str = f"{rule.label} -> {rule.word} "
+
+        # add the rule probability
+        rule_str += str(count/label_counter[rule.label]) 
+
+        # write the syntactic rule
+        lexicon_f.write(rule_str + "\n")
+    
+    # words
+    if grammar_prefix is None:
+        print("\nGrammar.words:")
+    for w in sorted(words):
+        words_f.write(w + "\n")
+    
+    # flush stdout or close the output files
+    if grammar_prefix is not None:
+        rules_f.close()
+        lexicon_f.close()
+        words_f.close()
+    else:
+        sys.stdout.flush()
+
+
         
-    
--- src/models.py
+++ src/models.py
@@ -1,34 +1,68 @@
 from typing import Union, List
 
 
+T = Terminal = str # terminal = word
+NT = NonTerminal = str # non-terminal = non-leaf tree node label
+A = Atom = T | NT  # atom = token = terminal or non-terminal
+
+
 class SymbolicExpression:
     # label: NonTerminal
-    # children: List[Union[Terminal, "SymbolicExpression"]] # [T] or [SE, SE, SE, ...]
+    # children: List[Union[Terminal, "SymbolicExpression"]] 
 
     def __init__(self, label: NonTerminal, children: List[Union[Terminal, "SymbolicExpression"]]):
         if not isinstance(label, NonTerminal):
             raise Exception("Parsing error: symbolic expression must start with a non-terminal!")
         if not children:
             raise Exception("Parsing error: symbolic expression contains only one element!")
+        if not(len(children) == 1 and isinstance(children[0], Terminal)):
+            for c in children:
+                if isinstance(c, Terminal):
+                    raise Exception("Parsing error: symbolic expression children must be either a single terminal or a non-empty list of non-terminals!")
+
 
         self.label = label
         self.children = children
 
-class Rule:
+    def __str__(self) -> str:
+        child_strs = []
+        for c in self.children:
+            if isinstance(c, SymbolicExpression):
+                child_strs.append(str(c))
+            else:
+                child_strs.append(c)
+        return f"({self.label} {' '.join(child_strs)})"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class SyntacticRule:
     # label: NonTerminal
     # children: list[NonTerminal]
 
-    def __init__(self, label: NonTerminal, children: list[NonTerminal]):
+    def __init__(self, label: NonTerminal, child_labels: list[NonTerminal]):
         if not isinstance(label, NonTerminal):
             raise Exception("Parsing error: rule label must be a non-terminal!")
-        if not children:
+        if not child_labels:
             raise Exception("Parsing error: rule contains only one element!")
 
         self.label = label
-        self.children = children
+        self.child_labels = child_labels
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, SyntacticRule)
+            and self.label == other.label
+            and self.child_labels == other.child_labels
+        )
+
+    def __hash__(self):
+        return hash((self.label, tuple(self.child_labels)))
 
 
-class Word:
+
+class LexicalRule:
     # label: NonTerminal
     # word: Terminal
     
@@ -41,13 +75,21 @@ class Word:
         self.label = label
         self.word = word
 
+    def __eq__(self, other):
+        return (
+            isinstance(other, LexicalRule)
+            and self.label == other.label
+            and self.word == other.word
+        )
 
-T = Terminal = str # terminal = word
-NT = NonTerminal = str # non-terminal = non-leaf tree node label
-A = Atom = T | NT  # atom = token = terminal or non-terminal
+    def __hash__(self):
+        return hash((self.label, self.word))
+
+
+
 SE = SymbolicExpression # symbolic expression = non-leaf tree node
-R = Rule # grammar rule
-W = Word # word with its grammar label (different labels possible)
+SR = SyntacticRule # grammar rule 
+LR = LexicalRule # grammar rule with a terminal
 
 
 
